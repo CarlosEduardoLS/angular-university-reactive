b916458eaf56c283c21b2739fd38d6a7d8999d91
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2022-07-20 12:58:55 -0300

    FEAT: Avoiding Angular duplicate HTTP requests

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { map, shareReplay } from "rxjs/operators";
 import { Course } from "../model/course";
 
 @Injectable()
@@ -9,8 +9,9 @@ export class CoursesService {
   constructor(private readonly http: HttpClient) {}
 
   findAll(): Observable<Course[]> {
-    return this.http
-      .get<Course[]>("/api/courses")
-      .pipe(map((res) => res["payload"]));
+    return this.http.get<Course[]>("/api/courses").pipe(
+      map((res) => res["payload"]),
+      shareReplay()
+    );
   }
 }
