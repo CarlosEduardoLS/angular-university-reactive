5987d90b55f7a3ed523d7e5fe0bcfc7f8d18c608
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2022-07-26 01:29:30 -0300

    FEAT: The Single Data Observable Pattern

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { Observable } from "rxjs";
+import { combineLatest, Observable } from "rxjs";
+import { map, tap } from "rxjs/operators";
 import { Course } from "../model/course";
 import { Lesson } from "../model/lesson";
 import { CoursesService } from "../services/courses.service";
+
+interface CourseData {
+  course: Course;
+  lessons: Lesson[];
+}
 
 @Component({
   selector: "course",
@@ -16,15 +22,23 @@ export class CourseComponent implements OnInit {
     private readonly coursesService: CoursesService
   ) {}
 
-  course$: Observable<Course>;
-
-  lessons$: Observable<Lesson[]>;
+  data$: Observable<CourseData>;
 
   ngOnInit() {
     const courseId = parseInt(this.route.snapshot.paramMap.get("courseId"));
 
-    this.course$ = this.coursesService.findById(courseId);
+    const course$ = this.coursesService.findById(courseId);
 
-    this.lessons$ = this.coursesService.findLessonsByCourseId(courseId);
+    const lessons$ = this.coursesService.findLessonsByCourseId(courseId);
+
+    this.data$ = combineLatest([course$, lessons$]).pipe(
+      map(([course, lessons]) => {
+        return {
+          course,
+          lessons,
+        };
+      }),
+      tap(console.log)
+    );
   }
 }
