048fd7646281fd390f4f032490037d4ba9ee464a
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2022-07-25 23:41:15 -0300

    FEAT: Authentication Store

---
+++ src/app/services/auth.store.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable } from "rxjs";
+import { map, shareReplay, tap } from "rxjs/operators";
+import { User } from "../model/user";
+
+@Injectable({
+  providedIn: "root",
+})
+export class AuthStore {
+  constructor(private readonly http: HttpClient) {
+    this.isLoggedIn$ = this.user$.pipe(map((user) => !!user));
+
+    this.isLoggedOut$ = this.isLoggedIn$.pipe(map((loggedIn) => !loggedIn));
+  }
+
+  private subject = new BehaviorSubject<User>(null);
+
+  user$: Observable<User> = this.subject.asObservable();
+
+  isLoggedIn$: Observable<boolean>;
+  isLoggedOut$: Observable<boolean>;
+
+  login(email: string, password: string): Observable<User> {
+    return this.http.post<User>("/api/login", { email, password }).pipe(
+      tap((user) => this.subject.next(user)),
+      shareReplay()
+    );
+  }
+
+  logout() {
+    this.subject.next(null);
+  }
+}
